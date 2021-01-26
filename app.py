23049173cec7b633fe12dcb8c7cd40d569a33393
guanroger <guanchernglin.gmail.com>
2021-01-26 23:15:12 +0800

    change port/ modify

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ app.debug=True
 @app.route("/", methods=['GET', 'POST'])
 def index():
     #value = request.json['V'][1]
-    value_A =request.json['A']
-    value_V =request.json['V']
+    data=request.get_json()
+    A=data['A']
+    V=data['V']
     location=[123, 456]
     floor="G"
     return jsonify({"x":location[0], "y": location[1], "floor": floor})
+    #return jsonify({"return":data})
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
