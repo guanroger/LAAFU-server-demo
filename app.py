73fb6cead7fc05b81d99c0d050a9b23108bd0f61
guanroger <guanchernglin.gmail.com>
2021-01-26 21:18:34 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+app.debug=True
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    #value = request.json['V'][1]
+    value_A =request.json['A']
+    value_V =request.json['V']
+    location=[123, 456]
+    floor="G"
+    return jsonify({"x":location[0], "y": location[1], "floor": floor})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
